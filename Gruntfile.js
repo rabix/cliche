4b03fcf005625e8fca776c8c9a04cf966bea0ead
Milica Kadic <milica.kadic.87@gmai.com>
2014-08-05 19:21:09 +0200

    persistent data added

--- Gruntfile.js
+++ Gruntfile.js
@@ -369,7 +369,8 @@ module.exports = function (grunt) {
                             'fonts/*',
 //                            'bower_components/*'
                             'bower_components/es5-shim/es5-shim.js',
-                            'bower_components/json3/lib/json3.min.js'
+                            'bower_components/json3/lib/json3.min.js',
+                            'data/*'
                         ]
                     },
                     {
